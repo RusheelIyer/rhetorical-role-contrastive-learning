dd0f8c81395b7e34892b99ae52544184043861fc
Rusheel Iyer <ge23caz@mytum.de>
2022-06-18 11:07:20 +0200

    space formatting

--- losses.py
+++ losses.py
@@ -2,24 +2,26 @@ import torch
 import torch.nn as nn
 
 class SupConLoss(nn.Module):
-
-	def __init__(self, temperature=0.07):
-		super(SupConLoss, self).__init__()
+    
+    def __init__(self, temperature=0.07):
+        super(SupConLoss, self).__init__()
         self.temperature = temperature
 
-	"""Compute loss for model. If both `labels` and `mask` are None,
-        it degenerates to SimCLR unsupervised loss:
-        https://arxiv.org/pdf/2002.05709.pdf
-        Args:
-            features: hidden vector of shape [bsz, n_views, ...].
-            labels: ground truth of shape [bsz].
-            mask: contrastive mask of shape [bsz, bsz], mask_{i,j}=1 if sample j
-                has the same class as sample i. Can be asymmetric.
-        Returns:
-            A loss scalar.
-        """
+	"""
+    Compute loss for model. If both `labels` and `mask` are None,
+    it degenerates to SimCLR unsupervised loss:
+    https://arxiv.org/pdf/2002.05709.pdf
+    Args:
+        features: hidden vector of shape [bsz, n_views, ...].
+        labels: ground truth of shape [bsz].
+        mask: contrastive mask of shape [bsz, bsz], mask_{i,j}=1 if sample j
+            has the same class as sample i. Can be asymmetric.
+    Returns:
+        A loss scalar.
+    """
     def forward(self, batch, features):
-    	device = (torch.device('cuda') if features.is_cuda else torch.device('cpu'))
+
+        device = (torch.device('cuda') if features.is_cuda else torch.device('cpu'))
 
         documents, sentences, _ = batch["input_ids"].shape
         labels = torch.Tensor(batch["label_ids"])
@@ -41,9 +43,10 @@ class SupConLoss(nn.Module):
         norms = torch.matmul(norms, norms.T)
 
         cos_similarity_matrix = torch.div(
-            torch.div(torch.matmul(features, features.T),
-                      norms
-                      ),
+            torch.div(
+                torch.matmul(features, features.T),
+                norms
+                ),
             0.07).to(device)
 
         numerator = torch.exp(cos_similarity_matrix * mask).to(device)
