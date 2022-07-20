16caf969925eca0e9704bdc4315f40cbec8f6aec
Rusheel Iyer <ge23caz@mytum.de>
2022-07-20 14:50:20 +0200

    eval_model logic for case of no memory bank

--- eval.py
+++ eval.py
@@ -45,7 +45,7 @@ def get_activation(name):
         activation[name] = output.detach()
     return hook
 
-def eval_model(model, eval_batches, device, task, contrastive, memory_bank, memory_bank_labels):
+def eval_model(model, eval_batches, device, task, contrastive, memory_bank = None, memory_bank_labels = None):
     
     # register a hook to receive activations after sentence_lstm layer
     model.sentence_lstm.register_forward_hook(get_activation('sentence_lstm'))
@@ -75,10 +75,13 @@ def eval_model(model, eval_batches, device, task, contrastive, memory_bank, memo
             # get the batches sentence embeddings
             # sentence_embeddings_batch = activation['sentence_lstm']
 
-            knn_predicted_labels, _ = kNN(memory_bank, memory_bank_labels, features, batch["label_ids"])
-            true_labels_batch, predicted_labels_batch = \
-            clear_and_map_padded_values(batch["label_ids"].view(-1), knn_predicted_labels.view(-1), task.labels)
-                #clear_and_map_padded_values(batch["label_ids"].view(-1), output["predicted_label"].view(-1), task.labels)
+            if(memory_bank == None or memory_bank_labels == None):
+                true_labels_batch, predicted_labels_batch = \
+                    clear_and_map_padded_values(batch["label_ids"].view(-1), output["predicted_label"].view(-1), task.labels)
+            else:
+                knn_predicted_labels, _ = kNN(memory_bank, memory_bank_labels, features, batch["label_ids"])
+                true_labels_batch, predicted_labels_batch = \
+                    clear_and_map_padded_values(batch["label_ids"].view(-1), knn_predicted_labels.view(-1), task.labels)
 
             docwise_true_labels.append(true_labels_batch)
             docwise_predicted_labels.append(predicted_labels_batch)
