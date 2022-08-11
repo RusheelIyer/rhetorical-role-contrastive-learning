94ff2770170db12bdbeffac6f81d363b7a1ce6d2
Rusheel Iyer <ge23caz@mytum.de>
2022-08-11 14:02:19 +0200

    train-test-split add test set

--- datasets/train-test-split.py
+++ datasets/train-test-split.py
@@ -3,9 +3,11 @@ import random
 
 file_name = 'vetclaims-all-data.json'
 number_train_samples = 65
+number_dev = 5
 
 train_path = 'vetclaims-train.json'
-test_path = 'vetclaims-dev.json'
+dev_path = 'vetclaims-dev.json'
+test_path = 'vetclaims-test.json'
 
 with open(file_name, 'r') as file:
     annotated_docs = json.load(file)
@@ -13,10 +15,18 @@ with open(file_name, 'r') as file:
 random.shuffle(annotated_docs)
 
 train_data = annotated_docs[:number_train_samples]
-test_data = annotated_docs[number_train_samples:]
+dev_data = annotated_docs[number_train_samples:number_train_samples+number_dev]
+test_data = annotated_docs[number_train_samples+number_dev:]
+
+print(len(train_data))
+print(len(dev_data))
+print(len(test_data))
 
 with open(train_path, 'w') as train_file:
     json.dump(train_data, train_file)
+
+with open(dev_path, 'w') as dev_file:
+    json.dump(dev_data, dev_file)
 
 with open(test_path, 'w') as test_file:
     json.dump(test_data, test_file)
